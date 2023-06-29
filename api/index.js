d7bb2b22760eb8e033cab36b82ede38c63939d2b
Harsh Kishor <harshkishor2002@gmail.com>
2023-06-29 16:15:41 +0530

    upload by Link complete

--- api/index.js
+++ api/index.js
@@ -2,14 +2,19 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const imageDownloader = require("image-downloader");
+const cookieParser = require("cookie-parser");
 const User = require("./models/User");
 
 require("dotenv").config();
 
 const app = express();
-const secret = process.env.SECRET;
+const jwtSecret = process.env.JWT_SECRET;
 const bcryptSalt = bcrypt.genSaltSync(10);
 app.use(express.json());
+app.use('/uploads', express.static(__dirname+'/uploads'))
+app.use(cookieParser());
 app.use(
 	cors({
 		credentials: true,
@@ -22,6 +27,33 @@ mongoose.connect(process.env.MONGO_URL);
 app.get("/test", (req, res) => {
 	res.json("test ok");
 });
+
+app.post("/login", async (req, res) => {
+	const { email, password } = req.body;
+	const foundUser = await User.findOne({ email });
+	if (foundUser) {
+		const passok = bcrypt.compareSync(password, foundUser.password);
+		if (passok) {
+			jwt.sign(
+				{ email: foundUser.email, id: foundUser._id },
+				jwtSecret,
+				{},
+				(err, token) => {
+					if (err) throw err;
+					res.cookie("token", token).json(foundUser);
+				}
+			);
+		} else {
+			res.status(422).json({ alert: "Invalid password" });
+		}
+	} else {
+		res.status(404).json({ alert: "User not found" });
+	}
+});
+
+app.post("/logout", (req, res) => {
+	res.clearCookie("token").json({ message: "Logout Successful" });
+})
 
 app.post("/signup", async (req, res) => {
 	const { name, email, password } = req.body;
@@ -43,6 +75,29 @@ app.post("/signup", async (req, res) => {
 		res.status(500).json({ alert: "Something went wrong" });
 	}
 });
+
+app.get("/profile", (req, res) => {
+	const { token } = req.cookies;
+	if (token) {
+		jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+			if (err) throw err;
+			const { name, email, _id } = await User.findById(userData.id);
+			res.json({ name, email, _id });
+		});
+	} else {
+		res.json(null);
+	}
+});
+
+app.post('/upload-by-link', async (req, res) => {
+	const {link}=req.body;
+	const newName = 'photo'+Date.now()+'.jpg';
+	await imageDownloader.image({
+		url: link,
+		dest: __dirname+'/uploads/'+newName
+	});
+	res.json(newName);
+})
 
 app.listen(3000, () => {
 	console.log("server is listening on port 3000");
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,18 +5,24 @@ import LoginPage from "./pages/LoginPage";
 import Layout from "./Layout";
 import SignupPage from "./pages/SignupPage";
 import axios from "axios";
+import { UserContext, UserContextProvider } from "./UserContext";
+import AccountPage from "./pages/AccountPage";
 
 axios.defaults.baseURL = "http://localhost:3000";
-
+axios.defaults.withCredentials = true;
 function App() {
 	return (
+		<UserContextProvider>
 		<Routes>
 			<Route path="/" element={<Layout />}>
 				<Route index element={<IndexPage />} />
 				<Route path="/login" element={<LoginPage />} />
 				<Route path="/signup" element={<SignupPage />} />
+				<Route path="/account/:subpage?" element={<AccountPage />} />
+				<Route path="/account/:subpage/:action" element={<AccountPage />} />
 			</Route>
 		</Routes>
+		</UserContextProvider>
 	);
 }
 
